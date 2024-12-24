9ea09d5dd09e9865ce9839ecf3c34ebfc3ad3f21
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2024-12-24 17:01:33 +0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,21 @@
 
+function Write-ToFile {
+    param (
+        [string]$FilePath,
+        [string]$Content
+    )
+
+    # Ghi nội dung vào file
+    Set-Content -Path $FilePath -Value $Content
+
+    # Xác nhận file đã được tạo
+    Write-Host "File đã được ghi tại $FilePath"
+}
+
 # Định nghĩa đường dẫn và nội dung file
 $content = @"
 This is malware
 "@
-Write-Host "Malware executed!!"
 
+# Gọi hàm để ghi nội dung vào file
+Write-ToFile -FilePath $filePath -Content $content
